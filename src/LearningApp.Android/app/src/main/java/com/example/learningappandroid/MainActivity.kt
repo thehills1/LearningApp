11265bcf411c7c2d2167475024276c684136841f
hills <azv.hills@gmail.com>
2024-12-05 22:16:11 +0300

    Login screen init.

--- src/LearningApp.Android/app/src/main/java/com/example/learningappandroid/MainActivity.kt
+++ src/LearningApp.Android/app/src/main/java/com/example/learningappandroid/MainActivity.kt
@@ -1,47 +1,32 @@
 package com.example.learningappandroid
 
+import LoginScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.learningappandroid.ui.theme.LearningAppAndroidTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    @OptIn(ExperimentalMaterial3Api::class)
+	override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LearningAppAndroidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+			Surface(
+				modifier = Modifier.fillMaxSize()
+			) {
+				// Показать экран входа
+				LoginScreen(
+					onLoginClick = { /* Логика авторизации */ },
+					onRegisterClick = { /* Логика регистрации */ }
+				)
+			}
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LearningAppAndroidTheme {
-        Greeting("Android")
     }
 }
